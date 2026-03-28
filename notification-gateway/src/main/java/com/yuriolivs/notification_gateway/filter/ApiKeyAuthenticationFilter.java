07c5ecf3cbeccb6f8cf18981e804a520e623014f
Yuri Oliveira <yuri.os.2004@outlook.com>
2026-03-28 15:21:19 -0300

    feat: add SecurityConfig to disable standard spring security options

--- notification-gateway/src/main/java/com/yuriolivs/notification_gateway/filter/ApiKeyAuthenticationFilter.java
+++ notification-gateway/src/main/java/com/yuriolivs/notification_gateway/filter/ApiKeyAuthenticationFilter.java
@@ -2,6 +2,7 @@ package com.yuriolivs.notification_gateway.filter;
 
 import com.yuriolivs.notification_gateway.properties.GatewayProperties;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.http.HttpStatus;
@@ -10,6 +11,7 @@ import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 @Component
+@EnableConfigurationProperties(GatewayProperties.class)
 @RequiredArgsConstructor
 public class ApiKeyAuthenticationFilter implements GlobalFilter {
     private final GatewayProperties gatewayProperties;
